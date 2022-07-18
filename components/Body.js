e4d0bb537a67c6e22ab666cfc70a81356d8cc2a5
KRUTARTH6774 <201901098@daiict.ac.in>
2022-07-18 19:36:09 +0530

    bug3

--- components/Body.js
+++ components/Body.js
@@ -13,22 +13,32 @@ const Body = () => {
     const fetchLink = async (link) => {
         const response = await fetch('https://api.shrtco.de/v2/shorten?url=' + link)
         const data = await response.json()
-        if(link === ''){
-            alert('input field can not be empty')
+        if (link === '') {
+            document.getElementById('bg-img').style.border = '5px solid #f46262'
+            document.getElementById('error-msg').style.display = 'flex'
+            document.getElementById('error-msg').innerHTML = 'Input field can not be empty :)'
+            // alert('input field can not be empty')
         }
-        else if(!data.ok){
-            alert('Wrong input :(')
-        }else{
+        else if (!data.ok) {
+            document.getElementById('bg-img').style.border = '5px solid red'
+            document.getElementById('error-msg').style.display = 'flex'
+            document.getElementById('error-msg').innerHTML = 'Wrong input :('
+            // alert('Wrong input :(')
+        } else {
             setLinks(dist => ({ ...dist, [link]: data.result.short_link }))
+            document.getElementById('bg-img').style.border = 'none'
+            document.getElementById('error-msg').style.display = 'none'
+            document.getElementById('error-msg').innerHTML = ''
         }
         setLink('')
         // setLinks(data.result.short_link)
     }
-    const CopytoClipboard = (key,value)=>{
+    const CopytoClipboard = (key, value) => {
         // console.log(value)
         navigator.clipboard.writeText(value)
         document.getElementById(key).innerHTML = 'Copied!'
         document.getElementById(key).style.background = 'black'
+        document.getElementById(key + 'h3').style.color = 'black'
     }
     // dist[link] = slink
 
@@ -48,17 +58,22 @@ const Body = () => {
                             height={450}
                         />
                     </div>
-                    <div className={styles.input}>
+                    <div id='bg-img' className={styles.input}>
                         <input type="text" value={link} onChange={(e) => { setLink(e.target.value) }} placeholder='Sharten a link here...' />
                         <button onClick={() => fetchLink(link)} type='submit'>Sharten it!</button>
                     </div>
+                    <p id='error-msg' style={{ display: 'none', justifyContent: "center", color: "red", fontWeight: "bold" }}></p>
                     <div >
                         <div className={styles.log}>
                             <ul>
                                 {Object.entries(slink).map(([key, value]) => <li key={key}>
-                                    <h3>{key}</h3>
-                                    <h3>{value}</h3>
-                                    <button id={key} onClick={()=>{CopytoClipboard(key,value)}}>copy</button>
+                                    <h3 style={{ width: "50%" }}>{key}</h3>
+                                    <h3 id={key + 'h3'} style={{
+                                        marginLeft: "0%",
+                                        position: "relative",
+                                        left: "5%"
+                                    }}>{value}</h3>
+                                    <button id={key} onClick={() => { CopytoClipboard(key, value) }}>copy</button>
                                 </li>)}
                             </ul>
                         </div>
