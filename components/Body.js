b8bade00b0e5dc0447c590f16c52c0fc89d40c77
KRUTARTH6774 <201901098@daiict.ac.in>
2022-07-18 14:03:08 +0530

    added cards

--- components/Body.js
+++ components/Body.js
@@ -3,53 +3,58 @@ import styles from '../styles/Home.module.css'
 import peopleworking from '../public/people-working.png'
 import Image from 'next/image'
 
+import Card from './Card'
+
 const Body = () => {
     return (
-        <main>
-            <div>
-                <div className={styles.heading}>
-                    <h1 style={{ marginBottom: "-2%", marginTop: "1%" }}>MORE THAN JUST</h1>
-                    <h1 style={{ marginTop: "0%" }}>SHORTER LINKS</h1>
-                </div>
-                <div className={styles.image}>
-                    <Image
-                        src={peopleworking}
-                        width={450}
-                        height={450}
-                    />
-                </div>
-                <div className={styles.input}>
-                    <input type="text" placeholder='Sharten a link here...' />
-                    <button type='submit'>Sharten it!</button>
-                </div>
-                <div >
-                    <div className={styles.log}>
-                        <ul>
-                            <li>
-                                <h3>xyz3424343242342@gmail.com</h3>
-                                <h3>xyz@gmail.com</h3>
-                                <button>copy</button>
-                            </li>
-                            <li>
-                                <h3>xyz3424343242342@gmail.com</h3>
-                                <h3>xyz@gmail.com</h3>
-                                <button>copy</button>
-                            </li>
-                            <li>
-                                <h3>xyz3424343242342@gmail.com</h3>
-                                <h3>xyz@gmail.com</h3>
-                                <button>copy</button>
-                            </li>
-                        </ul>
+        <>
+            <main>
+                <div>
+                    <div className={styles.heading}>
+                        <h1 style={{ marginBottom: "-2%", marginTop: "1%" }}>MORE THAN JUST</h1>
+                        <h1 style={{ marginTop: "0%" }}>SHORTER LINKS</h1>
                     </div>
-                    <div className={styles.info}>
-                        <h1>Advanced Statistics</h1>
-                        <h3>Track jow your links are performing across the web with</h3>
-                        <h3 style={{marginTop:"-2%"}}>our advanced statistics dashboard</h3>
+                    <div className={styles.image}>
+                        <Image
+                            src={peopleworking}
+                            width={450}
+                            height={450}
+                        />
+                    </div>
+                    <div className={styles.input}>
+                        <input type="text" placeholder='Sharten a link here...' />
+                        <button type='submit'>Sharten it!</button>
+                    </div>
+                    <div >
+                        <div className={styles.log}>
+                            <ul>
+                                <li>
+                                    <h3>xyz3424343242342@gmail.com</h3>
+                                    <h3>xyz@gmail.com</h3>
+                                    <button>copy</button>
+                                </li>
+                                <li>
+                                    <h3>xyz3424343242342@gmail.com</h3>
+                                    <h3>xyz@gmail.com</h3>
+                                    <button>copy</button>
+                                </li>
+                                <li>
+                                    <h3>xyz3424343242342@gmail.com</h3>
+                                    <h3>xyz@gmail.com</h3>
+                                    <button>copy</button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={styles.info}>
+                            <h1>Advanced Statistics</h1>
+                            <h3>Track jow your links are performing across the web with</h3>
+                            <h3 style={{ marginTop: "-2%" }}>our advanced statistics dashboard</h3>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+            <Card />
+        </>
     )
 }
 
---
+++ components/Card.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from '../styles/Cardspage.module.css'
+import Image from 'next/image'
+import BrandRecognition from '../public/icon-brand-recognition.svg'
+const Card = () => {
+    return (
+
+        <div className={styles.wrap}>
+
+            <div>
+                <div>
+                    <Image src={BrandRecognition} />
+                </div>
+                <div>
+                    <h2>Brand Recognition</h2>
+                    <h4>Boost your brand recognition with each click. Generuc links don't mean a thing.Branded links help instil confidence in your content.</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <Image src={BrandRecognition} />
+                </div>
+                <div>
+                    <h2>Brand Recognition</h2>
+                    <h4>Boost your brand recognition with each click. Generuc links don't mean a thing.Branded links help instil confidence in your content.</h4>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <Image src={BrandRecognition} />
+                </div>
+                <div>
+                    <h2>Brand Recognition</h2>
+                    <h4>Boost your brand recognition with each click. Generuc links don't mean a thing.Branded links help instil confidence in your content.</h4>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card
